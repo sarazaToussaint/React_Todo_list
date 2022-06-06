a3d31123d53cea18bbf6aa865aa7f0a2889f7c4a
sarazaToussaint <tmsaraza@gmail.com>
2022-06-06 18:32:59 +0200

    Add the apdate item feature

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -11,11 +11,19 @@ class InputTodo extends Component {
             title: e.target.value
           });
       };
+
+      handleSubmit = e => {
+        e.preventDefault();
+        this.props.addTodoProps(this.state.title);
+        this.setState({
+            title: ""
+          });
+      };
       
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input 
         type="text" placeholder="Add Todo..." 
         value={this.state.title} 
